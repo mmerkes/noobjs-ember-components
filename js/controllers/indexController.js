f92af4b027c140512220d369a6387bcd38ec4d2f
Empty Arsenal <matt.merkes@gmail.com>
2014-03-25 15:37:13 -0700

    Added chartjs donut component

--- js/controllers/indexController.js
+++ js/controllers/indexController.js
@@ -1,3 +1,22 @@
 App.IndexController = Ember.Controller.extend({
-  markers: [ [ 47.6130284, -122.3420645 ], [ 47.6390722, -122.3057244 ]]
+  markers: [ [ 47.6130284, -122.3420645 ], [ 47.6390722, -122.3057244 ]],
+
+  chartData: [
+    {
+        value: 20,
+        color:"#637b85"
+    },
+    {
+        value : 30,
+        color : "#2c9c69"
+    },
+    {
+        value : 40,
+        color : "#dbba34"
+    },
+    {
+        value : 10,
+        color : "#c62f29"
+    }
+  ]
 });
